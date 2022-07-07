66af359aa9247b584d7767529f1659039622f41c
jhsing24 <jerohsing@gmail.com>
2022-07-07 12:50:01 +0900

    Finished base code (no implementation of states yet)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncExp } from './components/IncExp';
+import { TransList } from './components/TransList';
+import { AddTrans } from './components/AddTrans';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncExp />
+        <TransList />
+        <AddTrans />
+      </div>
     </div>
   );
 }
